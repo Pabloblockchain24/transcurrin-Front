7aa141a4a6988e93653d117f8e56196e42da504c
pabloblockchain24 <pablo.arce.p@mail.pucv.cl>
2024-07-24 22:23:20 -0400

    2223

--- src/components/Commitments/Commitments.jsx
+++ src/components/Commitments/Commitments.jsx
@@ -7,9 +7,9 @@ function Commitments() {
             <section className='sectionContainer'>
                 <h1 className='sectionTitle'>COMPROMISO CON NUESTROS CLIENTES</h1>
                 <div className='sectionsArticles'>
-                    <CommitmentItem title="TRANSPARENCIA" description="En todo momento podras conocer el estado de tu servicio a traves de nuestra intranet, tiempos de salida y llegada, operadores a cargo, tarifas, etc." img="../../../transparencia.jpg" />
-                    <CommitmentItem title="RESPONSABILIDAD" description="En todo momento podras conocer el estado de tu servicio a traves de nuestra intranet, tiempos de salida y llegada, operadores a cargo, tarifas, etc." img="../assets/images/commitments/responsabilidad.jpg" />
-                    <CommitmentItem title="DISPONIBILIDAD" description="En todo momento podras conocer el estado de tu servicio a traves de nuestra intranet, tiempos de salida y llegada, operadores a cargo, tarifas, etc." img="../assets/disponibilidad.jpg" />
+                    <CommitmentItem title="TRANSPARENCIA" description="En todo momento podras conocer el estado de tu servicio a traves de nuestra intranet, tiempos de salida y llegada, operadores a cargo, tarifas, etc." img="../../../imgs/transparencia.jpg" />
+                    <CommitmentItem title="RESPONSABILIDAD" description="En todo momento podras conocer el estado de tu servicio a traves de nuestra intranet, tiempos de salida y llegada, operadores a cargo, tarifas, etc." img="../../../imgs/responsabilidad.jpg" />
+                    <CommitmentItem title="DISPONIBILIDAD" description="En todo momento podras conocer el estado de tu servicio a traves de nuestra intranet, tiempos de salida y llegada, operadores a cargo, tarifas, etc." img="../../../imgs/disponibilidad.jpg" />
                 </div>
             </section>
             <hr className='LineHr' />
