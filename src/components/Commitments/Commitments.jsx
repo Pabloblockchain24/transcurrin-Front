c5f67b5d5d45bfa967e51d79e8a08789b72a1004
pabloblockchain24 <pablo.arce.p@mail.pucv.cl>
2024-07-24 20:35:38 -0400

    trying routes 2035

--- src/components/Commitments/Commitments.jsx
+++ src/components/Commitments/Commitments.jsx
@@ -9,7 +9,7 @@ function Commitments() {
                 <div className='sectionsArticles'>
                     <CommitmentItem title="TRANSPARENCIA" description="En todo momento podras conocer el estado de tu servicio a traves de nuestra intranet, tiempos de salida y llegada, operadores a cargo, tarifas, etc." img="../../assets/images/commitments/transparencia.jpg" />
                     <CommitmentItem title="RESPONSABILIDAD" description="En todo momento podras conocer el estado de tu servicio a traves de nuestra intranet, tiempos de salida y llegada, operadores a cargo, tarifas, etc." img="../assets/images/commitments/responsabilidad.jpg" />
-                    <CommitmentItem title="DISPONIBILIDAD" description="En todo momento podras conocer el estado de tu servicio a traves de nuestra intranet, tiempos de salida y llegada, operadores a cargo, tarifas, etc." img="/assets/images/commitments/disponibilidad.jpg" />
+                    <CommitmentItem title="DISPONIBILIDAD" description="En todo momento podras conocer el estado de tu servicio a traves de nuestra intranet, tiempos de salida y llegada, operadores a cargo, tarifas, etc." img="/src/assets/images/commitments/disponibilidad.jpg" />
                 </div>
             </section>
             <hr className='LineHr' />
