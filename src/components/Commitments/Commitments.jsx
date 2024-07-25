537296739543646efafa0e8f512d27ba8f6964af
pabloblockchain24 <pablo.arce.p@mail.pucv.cl>
2024-07-24 20:57:57 -0400

    trying fix img routes

--- src/components/Commitments/Commitments.jsx
+++ src/components/Commitments/Commitments.jsx
@@ -1,12 +1,13 @@
 /*Import componenets*/
 import CommitmentItem from "../CommitmentsItem/CommitmentItem"
+
 function Commitments() {
     return (
         <>
             <section className='sectionContainer'>
                 <h1 className='sectionTitle'>COMPROMISO CON NUESTROS CLIENTES</h1>
                 <div className='sectionsArticles'>
-                    <CommitmentItem title="TRANSPARENCIA" description="En todo momento podras conocer el estado de tu servicio a traves de nuestra intranet, tiempos de salida y llegada, operadores a cargo, tarifas, etc." img="../assets/transparencia.jpg" />
+                    <CommitmentItem title="TRANSPARENCIA" description="En todo momento podras conocer el estado de tu servicio a traves de nuestra intranet, tiempos de salida y llegada, operadores a cargo, tarifas, etc." img="../../../public/transparencia.jpg" />
                     <CommitmentItem title="RESPONSABILIDAD" description="En todo momento podras conocer el estado de tu servicio a traves de nuestra intranet, tiempos de salida y llegada, operadores a cargo, tarifas, etc." img="../assets/images/commitments/responsabilidad.jpg" />
                     <CommitmentItem title="DISPONIBILIDAD" description="En todo momento podras conocer el estado de tu servicio a traves de nuestra intranet, tiempos de salida y llegada, operadores a cargo, tarifas, etc." img="../assets/disponibilidad.jpg" />
                 </div>
